e5f6221a776b9501ef4f5d2b79f74ff873fc04fd
drehnstrom <drehnstrom@yahoo.com>
2022-07-16 13:27:20 -0400

    Initial Commit

---
+++ space-invaders/main.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "allow-http" {
+  name        = "${var.project}-sec-group-allow-http"
+  description = "Enable HTTP Access"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP Access"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "vm" {
+  count         = var.instance_count
+  ami           = var.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow-http.id]
+
+  user_data = file("install_space-invaders.sh")
+
+  tags = {
+    Name = "${var.project}-server-${count.index}"
+  }
+}
---
+++ space-invaders/variables.tf
@@ -0,0 +1,33 @@
+variable "project" {
+  description = "The name of the current project."
+  type        = string
+  default     = "My Project"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "image_id" {
+  description = "The id of the machine image (AMI) to use for the server."
+  type        = string
+  default = "ami-0be2609ba883822ec"
+}
+
+variable "instance_type" {
+  description = "The size of the VM instances."
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_count" {
+  description = "Number of instances to provision."
+  type        = number
+  default     = 0
+
+  validation {
+    condition     = var.instance_count >= 0 && var.instance_count <= 5
+    error_message = "Instance count must be between 0 and 5."
+  }
+}
